e2586374915d64b4e2d3cb07296b0d24e0aa1d88
Øyvind Asplin <oyvind.asplin@gmail.com>
2025-08-28 14:19:35 +0200

    lagt til dokumentasjonsfiler og endret navngivning av workflowbilder

--- LangGraph/LG_States_2_prompt_chaining.py
+++ LangGraph/LG_States_2_prompt_chaining.py
@@ -1,14 +1,7 @@
-import os
-import getpass
-from PIL import Image
-import io
 from dotenv import load_dotenv                      # load .env variables
-from langsmith import traceable                     # trace in LangSmith
 from langchain_openai import ChatOpenAI             # OpenAI as llm
-from pydantic import BaseModel, Field               # Schema for structured output
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
-from IPython.display import Image, display
 
 load_dotenv()
 
@@ -67,15 +60,10 @@ workflow.add_edge("polish_joke", END)
 # Compile
 chain = workflow.compile()
 
-# Show workflow
-#display(Image(chain.get_graph().draw_mermaid_png())) #for Jupitern notebook
-# Save to file
+# Show workflow and save to file
 png_bytes = chain.get_graph().draw_mermaid_png()
-with open("joke_workflow.png", "wb") as f:
+with open("./LangGraph/LG_states_2_prompt_chaining_workflow.png", "wb") as f:
     f.write(png_bytes)
-
-print("✅ Workflow diagram saved as 'joke_workflow.png'")
-print("📁 Check your project folder and open the PNG file manually")
 
 # Invoke
 state = chain.invoke({"topic": "cats"})
